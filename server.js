f5aae8cb397bf61e44d21fafabfcd43b4ec2322e
John A Maloney <john.a.maloney@gmail.com>
2018-10-25 23:43:36 -0500

    Revert route handler

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ if (process.env.REACT_APP_ENV !=='localhost') {
   const path = require('path');
 app.use(express.static(path.join(__dirname, 'client/build')));
 
-app.get('*', (req, res) => {
+app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '/client/build/index.html'));
 });
 }
