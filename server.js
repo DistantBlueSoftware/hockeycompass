4b5887b37a7f844ea75b768f003821122aa851f2
John A Maloney <john.a.maloney@gmail.com>
2019-01-18 15:35:43 -0600

    Fix cannot GET error

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 'use strict'
 require('dotenv').config();
 const express = require('express');
+const path = require('path');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const router = require('./router');
@@ -29,11 +30,10 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 if (!process.env.REACT_APP_ENV || process.env.REACT_APP_ENV !=='localhost') {
-  const path = require('path');
-  app.use(express.static(path.join(__dirname, 'client/build')));
+  app.use(express.static('client/build'));
 
-  app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '/client/build/index.html'));
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '/client/build', 'index.html'));
   });
 }
 
