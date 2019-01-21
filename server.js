730d70f726d9238ee4d080c38e9d9df26f8da13d
John A Maloney <john.a.maloney@gmail.com>
2019-01-21 12:18:11 -0600

    More fun with server settings

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 if (!process.env.REACT_APP_ENV || process.env.REACT_APP_ENV !=='localhost') {
-  app.use(express.static('client/build'));
+  app.use(express.static(path.join(__dirname, 'client/build')));
 
   app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '/client/build/index.html'));
