36e0091007e4d3668caae97c646ac8d37d33ce3e
John A Maloney <john.a.maloney@gmail.com>
2019-03-19 22:36:10 -0500

    Updated nav styles

--- client/src/Navigation.jsx
+++ client/src/Navigation.jsx
@@ -1,23 +1,27 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { NavLink, withRouter } from 'react-router-dom';
-import logo from './logo.png';
+import styled from 'styled-components';
+import logo from './logo-nav.png';
 import * as actions from './actions';
 
-const navStyle = {
-  backgroundColor: 'linear-gradient(rgba(0,0,0,0.3),rgba(0,0,0,0.6))',
-  position: 'fixed',
-  top: 0,
-  width: '100vw',
-  zIndex: 1000,
-  height: '80px',
-  borderBottom: '1px solid rgba(255,255,255,0.5)',
-  transition: 'all 0.3s'
-}
+const Navbar = styled.nav`
+  background-color: linear-gradient(rgba(0,0,0,0.3),rgba(0,0,0,0.6));
+  position: fixed;
+  top: 0;
+  width: 100vw;
+  z-index: 1000;
+  border-bottom: 1px solid rgba(255,255,255,0.5);
+  transition: all 0.3s;
+`
 
 const mapStateToProps = state => {
   console.log(state)
   return state;
+}
+
+const toggleNavColor = () => {
+  
 }
 
 
@@ -25,9 +29,11 @@ const Navigation = ({user, doLogout, location}) => {
   console.log(location.pathname)
   const navColor = location.pathname === '/' ? 'transparent' : 'rgb(25, 81, 139)';
   return (
-    <nav style={{...navStyle, background: navColor}} className='navbar navbar-expand-lg navbar-light'>
-      <NavLink className='navbar-brand' to='/' style={{paddingTop: '1rem'}}><img src={logo} width={'128px'} alt='Hockey Compass' /></NavLink>
-      <button className='navbar-toggler' type='button' data-toggle='collapse' data-target='#navbarSupportedContent' aria-controls='navbarSupportedContent' aria-expanded='false' aria-label='Toggle navigation'>
+    <Navbar style={{background: navColor}} className='navbar navbar-expand-lg navbar-dark'>
+      <NavLink to='/' style={{paddingTop: '1rem'}}>
+        <img src={logo} alt='Hockey Compass' />
+      </NavLink>
+      <button className='navbar-toggler' style={{border: 'none'}} type='button' data-toggle='collapse' data-target='#navbarSupportedContent' aria-controls='navbarSupportedContent' aria-expanded='false' aria-label='Toggle navigation' onClick={e => toggleNavColor()}>
       <span className='navbar-toggler-icon'></span>
     </button>
     <div className='collapse navbar-collapse' id='navbarSupportedContent'>
@@ -54,7 +60,7 @@ const Navigation = ({user, doLogout, location}) => {
         </React.Fragment>
       }
     </div>
-    </nav>
+    </Navbar>
 )
 }
 export default connect(mapStateToProps, actions)(withRouter(Navigation));
