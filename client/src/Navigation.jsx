26623ce393ea0e155bbcceaeb167d29071e6b475
John A Maloney <john.a.maloney@gmail.com>
2019-03-19 22:44:58 -0500

    Updated nav styles in lifecycle

--- client/src/Navigation.jsx
+++ client/src/Navigation.jsx
@@ -25,42 +25,58 @@ const toggleNavColor = () => {
 }
 
 
-const Navigation = ({user, doLogout, location}) => {
-  console.log(location.pathname)
-  const navColor = location.pathname === '/' ? 'transparent' : 'rgb(25, 81, 139)';
-  return (
-    <Navbar style={{background: navColor}} className='navbar navbar-expand-lg navbar-dark'>
-      <NavLink to='/' style={{paddingTop: '1rem'}}>
-        <img src={logo} alt='Hockey Compass' />
-      </NavLink>
-      <button className='navbar-toggler' style={{border: 'none'}} type='button' data-toggle='collapse' data-target='#navbarSupportedContent' aria-controls='navbarSupportedContent' aria-expanded='false' aria-label='Toggle navigation' onClick={e => toggleNavColor()}>
-      <span className='navbar-toggler-icon'></span>
-    </button>
-    <div className='collapse navbar-collapse' id='navbarSupportedContent'>
-      <ul className='navbar-nav mr-auto'>
-        <NavLink className='nav-link' to='/'><li className='nav-item'>Home</li></NavLink>
-        <NavLink className='nav-link' to='/games'><li className='nav-item'>Games</li></NavLink>
-        <NavLink className='nav-link' to='/venues'><li className='nav-item'>Venues</li></NavLink>
-        {user.authenticated && 
+class Navigation extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      navColor: this.props.location.pathname === '/' ? 'transparent' : 'rgb(25, 81, 139)'
+    }
+  }
+  
+  componentDidUpdate(prevProps, prevState, snapshot) {
+    if (prevProps.location.pathname !== this.props.location.pathname){
+    this.setState({
+      navColor: this.props.location.pathname === '/' ? 'transparent' : 'rgb(25, 81, 139)'
+    })}
+  }
+  
+  render() {
+    const {user, doLogout, location} = this.props;
+    const { navColor } = this.state;
+    return (
+      <Navbar style={{background: navColor}} className='navbar navbar-expand-lg navbar-dark'>
+        <NavLink to='/' style={{paddingTop: '1rem'}}>
+          <img src={logo} alt='Hockey Compass' />
+        </NavLink>
+        <button className='navbar-toggler' style={{border: 'none'}} type='button' data-toggle='collapse' data-target='#navbarSupportedContent' aria-controls='navbarSupportedContent' aria-expanded='false' aria-label='Toggle navigation' onClick={e => toggleNavColor()}>
+        <span className='navbar-toggler-icon'></span>
+      </button>
+      <div className='collapse navbar-collapse' id='navbarSupportedContent'>
+        <ul className='navbar-nav mr-auto'>
+          <NavLink className='nav-link' to='/'><li className='nav-item'>Home</li></NavLink>
+          <NavLink className='nav-link' to='/games'><li className='nav-item'>Games</li></NavLink>
+          <NavLink className='nav-link' to='/venues'><li className='nav-item'>Venues</li></NavLink>
+          {user.authenticated && 
+            <React.Fragment>
+            <NavLink className='nav-link' to='/profile'><li className='nav-item'>Profile</li></NavLink>
+            {user.role === 1 && <NavLink className='nav-link' to='/admin'><li className='nav-item'>Admin</li></NavLink>}
+            </React.Fragment>
+          }
+          
+        </ul>
+        {user.authenticated ?
           <React.Fragment>
-          <NavLink className='nav-link' to='/profile'><li className='nav-item'>Profile</li></NavLink>
-          {user.role === 1 && <NavLink className='nav-link' to='/admin'><li className='nav-item'>Admin</li></NavLink>}
+            <span className='navbar-text' style={{marginRight: '20px', color: 'white'}}>Welcome, {user.username}</span>
+            <button className='btn btn-primary' type='button' onClick={doLogout}>Logout</button>
+          </React.Fragment> :
+          <React.Fragment>
+            <NavLink className='nav-link' to='/login'><span>Login</span></NavLink>
+            <NavLink className='nav-link' to='/register'><span>Register</span></NavLink>
           </React.Fragment>
         }
-        
-      </ul>
-      {user.authenticated ?
-        <React.Fragment>
-          <span className='navbar-text' style={{marginRight: '20px', color: 'white'}}>Welcome, {user.username}</span>
-          <button className='btn btn-primary' type='button' onClick={doLogout}>Logout</button>
-        </React.Fragment> :
-        <React.Fragment>
-          <NavLink className='nav-link' to='/login'><span>Login</span></NavLink>
-          <NavLink className='nav-link' to='/register'><span>Register</span></NavLink>
-        </React.Fragment>
-      }
-    </div>
-    </Navbar>
-)
+      </div>
+      </Navbar>
+    )
+  }
 }
 export default connect(mapStateToProps, actions)(withRouter(Navigation));
