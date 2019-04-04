aa0d7b68cfe4951c65258958b947f17f4f91efb0
John A Maloney <john.a.maloney@gmail.com>
2019-04-04 15:13:43 -0500

    Updated footer

--- client/src/Footer.jsx
+++ client/src/Footer.jsx
@@ -2,5 +2,8 @@ import React from 'react'
 import moment from 'moment'
 
 export const Footer = () => (
-  <div style={{fontSize: '10px'}}>&copy;{moment().format('YYYY')} hockeycompass all rights reserved</div>
+  <div style={{display: 'flex', justifyContent: 'space-between', margin: '10px', width: '96vw'}}>
+    <div style={{fontSize: '10px'}}>&copy;{moment().format('YYYY')} hockeycompass all rights reserved</div>
+    <div style={{fontSize: '10px'}}>built by <a href='https://distantbluesoftware.com'>distantblue software</a></div>
+  </div>
 )
