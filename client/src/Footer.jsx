a8cc9a2fe8e451702a77e9355be5d5c0a1a45105
John A Maloney <john.a.maloney@gmail.com>
2019-04-24 08:50:51 -0500

    Updated copyright

--- client/src/Footer.jsx
+++ client/src/Footer.jsx
@@ -3,7 +3,7 @@ import moment from 'moment'
 
 export const Footer = () => (
   <div style={{display: 'flex', justifyContent: 'space-between', margin: '10px', width: '96vw'}}>
-    <div style={{fontSize: '10px'}}>&copy;{moment().format('YYYY')} hockeycompass all rights reserved</div>
+    <div style={{fontSize: '10px'}}>&copy;{moment().format('YYYY')} Hockeycompass, LLC. All rights reserved.</div>
     <div style={{fontSize: '10px'}}>built by <a href='https://distantbluesoftware.com'>distantblue software</a></div>
   </div>
 )
